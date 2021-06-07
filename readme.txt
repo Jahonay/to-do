d56d50c7dc900069f8ad8e48827a7b813be90187
xjmackey <xjmackey@gmail.com>
2021-06-07 14:16:19 -0400

    initial add

---
+++ readme.txt
@@ -0,0 +1,14 @@
+//                         To do                           \\
+
+This is going to be a silly little to-do list app using 
+vanilla html, css & javascript. I'm hoping to make it both 
+functional but also aesthetically pleasing. 
+
+I want it to be able to add to-do list objects, delete to do
+list objects, display them, delete them all, add more, and
+maybe even color code them. 
+
+As always, let me know what you would change, how you would
+have done it differently, and what you want to see from me
+in the future. 
+\\                                                         //
---
+++ to-do.js
@@ -0,0 +1,81 @@
+let todoArray = [];
+
+const mainContainer = document.querySelector('.content');
+const header = querySelector("#title-input");
+const description = querySelector("#description-input");
+const add = querySelector("#add");
+const del = querySelector('.close');
+
+let tableParent, tableDisplay, todoArray, todoCounter,
+ todoDelete, todoDescription, todoTitle;
+
+//create a table parent
+window.onLoad = function(){
+    tableParent= document.createElement('ul');
+    tableParent.classList.add('tableContainer');
+    mainContainer.appendChild(tableParent);
+    //tableParent.style.display="none";
+}
+
+//
+// create the actual list table
+//
+
+function createTodoTable() {
+    tableDisplay = document.createElement('li');
+    todoCounter = document.createElement('span');
+    todoTitle = document.createElement('span');
+    todoDescription = document.createElement('span');
+    todoDelete = document.createElement('span');
+
+    //adding classes
+    tableDisplay.classList.add('tableDisplay');
+    todoCounter.classList.add('todoCounter');
+    todoTitle.classList.add('todoTitle');
+    todoDescription.classList.add('todoDescription');
+
+    todoDelete.classList.add('todoDelete');
+}
+
+function print(){
+    tableParent.innerHTML = "";
+
+    function removeElement(i){
+        todoArray = todoArray.filter( ( _, z ) => z != i );
+        printArray();
+    }
+
+    todoArray.map((value, i) =>{
+        createTableData ();
+
+        noteCounter.textContent = i+1;
+        todoTitle.innerText=value.heading;
+        todoDescription.innerText = value.content;
+
+        tableDisplay.appendChild(todoCounter);
+        tableDisplay.appendChild(todoTitle);
+        tableDisplay.appendChild(todoDescription);
+        tableDisplay.appendChild(todoDelete);
+
+        todoDelete.addEventListener("click", () => removeElement(i));
+
+        tableParent.appendChild(tableDisplay);
+    });
+
+
+}
+
+add.addEventListener("click", function () {
+    if (header.value !== "" && description.value !== ""){
+        let tempObj = {};
+        tempObj.heading= header.value;
+        tempObj.content=description.value;
+
+        todoArray= [...todoArray, tempObj];
+        printArray();
+        tableParent.style.display ="block";
+    } else{
+        alert("Please, Enter your to-do title and description")
+    }
+    reset();
+});
